de9f1cd47bbe139d5c54fc70d43b9242f3e6056d
albertgoncalves <goncalves.albert@gmail.com>
2019-11-24 20:20:57 -0500

    render map in addition to grid and noise

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from random import seed
 from time import time
 from sys import argv
 
+from matplotlib.colors import BoundaryNorm, ListedColormap
 from matplotlib.pyplot import arrow, close, savefig, subplots, tight_layout
 from numpy import max, min
 
@@ -14,7 +15,7 @@ from noise import iterate, normalize
 WD = environ["WD"]
 
 
-def args():
+def parse_args():
     try:
         return (
             int(argv[1]),
@@ -46,6 +47,12 @@ def pad_axis(ax, xs, ys, k):
     pad = y_pad if x_pad < y_pad else x_pad
     ax.set_xlim([x_min - pad, x_max + pad])
     ax.set_ylim([y_min - pad, y_max + pad])
+
+
+def export(filename):
+    tight_layout()
+    savefig(filename)
+    close()
 
 
 def plot_grid(
@@ -82,18 +89,50 @@ def plot_grid(
         )
     pad_axis(ax, xs, ys, pad)
     ax.set_aspect("equal")
-    tight_layout()
-    savefig(filename)
-    close()
+    export(filename)
 
 
 def plot_noise(zs, figsize, filename):
     _, ax = subplots(figsize=figsize)
     ax.matshow(zs, cmap="bone")
     ax.invert_yaxis()
-    tight_layout()
-    savefig(filename)
-    close()
+    export(filename)
+
+
+def plot_map(zs, figsize, filename):
+    cmap = ListedColormap([
+        "#263878",
+        "#324ca8",
+        "#4362d1",
+        "#dbc984",
+        "wheat",
+        "moccasin",
+        "tan",
+        "#a3855a",
+        "forestgreen",
+        "green",
+        "darkgreen",
+        "#17541a",
+    ])
+    norm = BoundaryNorm([
+        0,
+        0.315,
+        0.43,
+        0.5,
+        0.525,
+        0.55,
+        0.61,
+        0.65,
+        0.7,
+        0.7575,
+        0.825,
+        0.915,
+        1,
+    ], cmap.N, clip=True)
+    _, ax = subplots(figsize=figsize)
+    ax.matshow(zs, cmap=cmap, norm=norm)
+    ax.invert_yaxis()
+    export(filename)
 
 
 def filepath(filename):
@@ -108,7 +147,8 @@ def timer(label, function, *args, **kwargs):
 
 
 def main():
-    (s, n_col, n_row, resolution, octaves, persistence, figsize, pad) = args()
+    (s, n_col, n_row, resolution, octaves, persistence, figsize, pad) = \
+        parse_args()
     seed(s)
     n = n_col * n_row
     (xs, ys, vxs, vys) = timer("grid.init(...)", init, n, n_col, n_row)
@@ -140,12 +180,18 @@ def main():
         octaves,
         persistence,
     )
+    args = (normalize(zs).reshape(res_n_row, res_n_col), figsize)
     timer(
         "main.plot_noise(...)",
         plot_noise,
-        zs.reshape(res_n_row, res_n_col),
-        figsize,
+        *args,
         filepath("noise.png"),
+    )
+    timer(
+        "main.plot_map(...)",
+        plot_map,
+        *args,
+        filepath("map.png"),
     )
 
 
