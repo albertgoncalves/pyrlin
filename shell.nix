817a4268213a48a00413d9f39f3ff6c765c8563d
albertgoncalves <goncalves.albert@gmail.com>
2019-12-18 22:50:23 -0500

    update python version

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 with import <nixpkgs> {};
 mkShell {
     buildInputs = [
-        (python37.withPackages(ps: with ps; [
+        (python38.withPackages(ps: with ps; [
             flake8
             matplotlib
             numba
             numpy
+            setuptools
         ]))
         shellcheck
     ];
